e3332fd385c572287e23ed98eadb68d434ca8380
Totti <toti.clash9@gmail.com>
2026-02-28 18:04:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -394,7 +394,9 @@ const lines = [header.join(separator)].concat(
   })
 );
 
-    const blob = new Blob([lines.join("\n")], {type:"text/csv"});
+    const blob = new Blob(["\ufeff" + lines.join("\n")], {
+  type: "text/csv;charset=utf-8;"
+});
     const url = URL.createObjectURL(blob);
     const a = document.createElement("a");
     a.href = url;
